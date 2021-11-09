e5c1669a316a0a38f5815f5c601cd7c56820408d
LoiseAJM <loise.mauguin@gmail.com>
2021-11-09 15:06:24 +0100

    Créations  footer et header, inclusion dans Accueil

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Footer</title>
+        <link rel="stylesheet" href="style.css">
+        <!--Favicon-->
+        <link rel="icon" type="image" href="images/favicon-gbaf.png" />
+    </head> 
+    <footer>
+        <!--Lien vers les mentions légales-->
+        <a href="mentionslegales.html"> Mentions légales 
+    </footer>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Header</title>
+        <link rel="stylesheet" href="style.css">
+        <!--Favicon-->
+        <link rel="icon" type="image" href="images/favicon-gbaf.png" />
+
+    </head> 
+    <header>
+                <!--Logo GBAF-->
+            <img id="logoGBAF" src="images/logo_gbaf.png"/>
+
+    </header>
+</html>
