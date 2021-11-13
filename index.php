ae8ff6c8cd8672a9b7a07cf4f84ddddc048d2c70
LoiseAJM <loise.mauguin@gmail.com>
2021-11-13 08:46:17 +0100

    Plan site

--- index.php
+++ index.php
@@ -8,46 +8,34 @@
     </head>
     <body>
     <!--Appel du header-->
-    <?php require 'header.php'; ?>
-        <div class="conteneur">
-        <div class="formulaire">
-            <h1 id="titrecontact">
-                Créer un compte
-            </h1>
-            <form>
-                <div>
-                    <label for="nom">Nom : </label>
-                    <input type="text" name="nom" id="nom" placeholder="Votre nom" class="form_input">
-                </div>
-                
-                <div>
-                    <label for="prenom">Prénom :</label>
-                    <input type="text" name="prenom" id="prenom" placeholder="Votre prénom" class="form_input">
-                </div>
-                <div >
-                    <label for="username">Identifiant :</label>
-                    <input type="text" name="username" id="username" placeholder="Username" class="form_input">
-                </div>
-                <div>
-                    <label for="motdepasse">Mot de passe :</label>
-                    <input type="password" name="nom" id="motdepasse" placeholder="Mot de passe" class="form_input">
-                </div>
-                <div >
-                    <label for="questionsecrete">Question secrète :</label>
-                    <input type="text" name="questionsecrete" id="questionsecrete" placeholder="Question secrète" class="form_input">
-                </div>
-                <div >
-                    <label for="username">Réponse :</label>
-                    <input type="text" name="reponse" id="reponse" placeholder="Réponse" class="form_input">
-                </div>
-                <div>
-                    <button class="submit">Envoyer
-                    </button>
-                </div>
-        </div>
+        <?php require 'header.php'; ?>
+            <div class="conteneur1">
+                <div class="formulaire1">
+                    <h1 id="connexion">
+                        Connexion
+                    </h1>
+                    <form>
+                        <div>
+                            <label for="Identifiant"> Identifiant :</label>
+                            <input type="text" name="nom" id="nom" placeholder="Votre nom" class="form_input">
+                        </div>
+                        
+                        <div>
+                            <label for="prenom">Mot de passe :</label>
+                            <input type="text" name="prenom" id="prenom" placeholder="Votre prénom" class="form_input">
+                        </div>
 
-            </form>
-        </div>
+                        <div>
+                            <button class="submit">Envoyer
+                            </button>
+                        </div>
+                    </form>
+                </div>  
+                <div>
+                <a href="connexion.php"><button type="button">Je n'ai pas de compte</button></a>
+
+                </div>
+            </div>
         <?php require 'footer.php'; ?>
     </body>
 </html>
