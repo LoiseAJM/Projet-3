a6ff4cec1ba2a977c6540b49be48efdffcbd68cc
LoiseAJM <loise.mauguin@gmail.com>
2021-12-01 09:37:28 +0100

    Update redirection_recuperation_identifiant.php

--- redirection_recuperation_identifiant.php
+++ redirection_recuperation_identifiant.php
@@ -4,9 +4,10 @@
 </head>
 <?php require 'header.php'; ?>
 <div class="boxed">
-<p> Votre identifiant est : <?php echo $_SESSION['identifiant'] ;?> </p>
+<p> Votre identifiant est : <bold class="redbold"> <?php echo $_SESSION['identifiant']?></bold> </p>
+<meta http-equiv="refresh" content="5;index.php">
 <?php session_destroy(); ?>
-<p> Vous allez être redirigé vers la page de connexion, si la redirection ne fonctionne pas,  cliquez ici : <a href="index.php">Connexion</a></p>
+<p>Vous allez être redirigé vers la page de connexion dans 5 secondes, si la redirection ne fonctionne pas,  cliquez ici : <a href="index.php">Connexion</a></p>
 <?php require 'footer.php'?>
 </div>
 
